4c5a140b4492600eb898c4e0a9ceabc4f21c0f18
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-16 21:51:08 +0530

    Styles added to the navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const Navigation: React.FC = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="https://github.com/Sam44323/REACT-TS-JSON-DEMO">Code</a>
-        </li>
-      </ul>
-    </div>
+    <ul className="flex justify-around py-4 bg-gray-200">
+      <li className="inline-block bg-red-100 px-4 rounded hover:bg-red-500 cursor-pointer hover:text-gray-100 transition ease-out duration:300 shadow-lg">
+        <a href="/">Home</a>
+      </li>
+      <li className="inline-block bg-red-100 px-4 rounded hover:bg-red-500 cursor-pointer hover:text-gray-100 transition ease-out duration:300 shadow-lg">
+        <a href="https://github.com/Sam44323/REACT-TS-JSON-DEMO">Code</a>
+      </li>
+    </ul>
   );
 };
 
