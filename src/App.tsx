34ee333271d8079be00b63dee379a2699aa1b718
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-16 22:04:21 +0530

    Routes for compoents added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import "./App.css";
+import React, { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router";
 
 import ListContainer from "./containers/ListContainer";
 import Navigation from "./components/Navigation";
+
+const ItemDetails = lazy(() => import("./components/ItemDetail"));
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <Navigation />
-      <ListContainer />
+      <Suspense fallback={<h1 className="mt-9 text-center">Loading...</h1>}>
+        <Switch>
+          <Route path="/" exact component={ListContainer} />
+          <Route path="/details/:id" component={ItemDetails} />
+        </Switch>
+      </Suspense>
     </React.Fragment>
   );
 };
