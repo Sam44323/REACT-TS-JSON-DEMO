1e2fcccff6d84dee135de6e837a83eb796713a65
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-17 13:38:26 +0530

    Replaced a with Link in navigation for home

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation: React.FC = () => {
   return (
     <ul className="flex justify-around py-4 bg-gray-200">
       <li className="inline-block bg-red-100 px-4 rounded hover:bg-red-500 cursor-pointer hover:text-gray-100 transition ease-out duration:300 shadow-lg">
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li className="inline-block bg-red-100 px-4 rounded hover:bg-red-500 cursor-pointer hover:text-gray-100 transition ease-out duration:300 shadow-lg">
         <a href="https://github.com/Sam44323/REACT-TS-JSON-DEMO">Code</a>
