6fb85744700033411499a4e17618a947c109f849
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-17 20:55:56 +0530

    Updated the testing method for the ListContainer component

--- src/containers/ListContainer.test.jsx
+++ src/containers/ListContainer.test.jsx
@@ -1,9 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import ListContainer from "./ListContainer";
+import { getQueriesForElement } from "@testing-library/react";
 
 test("check for the rendering of loading", () => {
   const root = document.createElement("div");
   ReactDOM.render(<ListContainer />, root);
-  expect(root.querySelector("div").textContent).toBe("Loading...");
+
+  const { getByText } = getQueriesForElement(root);
+  expect(getByText("Loading...")).not.toBeNull();
 });
